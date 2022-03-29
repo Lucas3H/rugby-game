03ea4bd6a2a519e5191fe6c4a53ddce12929c20f
Lucas Harada <lucas.harada1900@gmail.com>
2022-03-29 15:44:50 -0300

    Defender strategy made

--- src/defender.c
+++ src/defender.c
@@ -16,14 +16,32 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+static int rounds = 0;
+static size_t final_row;
+
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+  	// TODO: unused parameters, remove these lines later
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+	rounds++;
+	if(rounds < 5){
+		if(rounds%2 == 0) 
+			return (direction_t) DIR_UP;
+		else 
+			return (direction_t) DIR_DOWN;
+	}
+	if(rounds == 5){
+		position_t attacker_position = get_spy_position(attacker_spy);
+		if(attacker_position.i <= 4) 
+			final_row = 3;
+		else
+			final_row = 7;
+	}
+
+	if(final_row <= defender_position.i)
+		return (direction_t) DIR_UP;
+	else
+		return (direction_t) DIR_DOWN;
 }
 
 /*----------------------------------------------------------------------------*/
